49f74e6fc85e6d1ae443ade7f93a928369d0304c
agustin conte <agustinconte15@gmail.com>
2023-02-01 13:13:16 -0300

    tp 2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,57 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    let numero1;
+	let numero2;
+	let suma;
+    let Rectangulo;
+    let alambre;
+	let mensaje;
 
+	numero1 = parseInt(document.getElementById("txtIdLargo").value);
+	numero2 = parseInt(document.getElementById("txtIdAncho").value);
+  
+	suma = numero1 + numero2;
+    Rectangulo = suma * 2;
+    alambre = Rectangulo * 3;
+
+	mensaje = "Hace falta  "+ alambre +" metros de alambre";
+
+	alert(mensaje);
 }
 function Circulo () 
 {
-	
+	let radio;
+	let perimetro;
+    let alambre;
+	let mensaje;
+
+	radio = parseInt(document.getElementById("txtIdRadio").value);
+  
+	perimetro = radio * 2 * 3.14;
+    alambre = perimetro * 3;
+
+	mensaje = "Hace falta  "+ alambre +" metros de alambre";
+
+	alert(mensaje);
 }
 function Materiales () 
 {
-	
+	let numero1;
+	let numero2;
+	let m2;
+    let cal;
+    let cemento;
+	let mensaje;
+
+	numero1 = parseFloat(document.getElementById("txtIdLargo").value);
+	numero2 = parseFloat(document.getElementById("txtIdAncho").value);
+  
+	m2 = numero1 * numero2;
+    cemento= m2 * 2;
+    cal = m2 * 3;
+
+	mensaje = "Hace falta "+ cemento +" bolsas de cemento y "+ cal +" bolsas de cal";
+
+	alert(mensaje);
 }
