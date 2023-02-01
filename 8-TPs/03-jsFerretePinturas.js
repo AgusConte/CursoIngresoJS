4c08dbde93552cfb21c69348ac056fadb5321b64
agustin conte <agustinconte15@gmail.com>
2023-02-01 13:31:33 -0300

    ejercicio 3

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,28 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+	let Fahrenheit;
+	let celcius;
+	let mensaje;
+
+	Fahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+	celcius = (Fahrenheit - 32) * 5 / 9
+	mensaje = Fahrenheit +"º fahrenheit son "+ celcius +"º celcius" ;
+
+	alert(mensaje)
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	let Fahrenheit;
+	let celcius;
+	let mensaje;
+
+	Fahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+	celcius = (Fahrenheit * 9 / 5) + 32 
+	mensaje = celcius +"º celcius son "+ Fahrenheit +"º fahrenheit" ;
+
+	alert(mensaje)
 }
