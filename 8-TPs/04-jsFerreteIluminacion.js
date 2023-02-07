2f52d8b02f2b92833afcfa88addba64b0e200219
agustin conte <agustinconte15@gmail.com>
2023-02-07 11:15:32 -0300

    falta e

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,67 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    let cantidadLamparas;
+    let marca;
+    let lampara;
+    let descuento;
+    let precio;
+    let impuesto;
+
+
+
+    cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value)
+    marca = document.getElementById("Marca").value
+    impuesto =
+
+
+    lampara = 35
+
+
+
+
+
+ 	if(cantidadLamparas >= 6) {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.5);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 5 &&  marca == "ArgentinaLuz") 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.4);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 5 ) 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.3);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 4 &&  marca == "ArgentinaLuz" ||  cantidadLamparas ==4 && marca == "FelipeLamparas" ) 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.25);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 4 ) 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.2);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 3 &&  marca == "ArgentinaLuz" ) 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.15);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 3 &&  marca == "FelipeLamparas" ) 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.10);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(cantidadLamparas == 3) 
+    {precio = (lampara * cantidadLamparas);
+        descuento = precio - (precio * 0.05);
+    (document.getElementById("txtIdprecioDescuento").value) = descuento}
+
+    else if(precio > 120)
+    {impuesto = precio + (precio * 0.1);
+        (document.getElementById("txtIdprecioDescuento").value) = impuesto
+    alert("Usted pago "+ impuesto +" de IIBB.")}
 }
