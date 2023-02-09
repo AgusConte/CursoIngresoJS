281d44aa02ef49f0ac971ab91b270ef011501374
agustin conte <agustinconte15@gmail.com>
2023-02-09 12:14:20 -0300

    6

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -1,7 +1,36 @@
 function mostrar()
 {
 	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
+	let horaDelDia = document.getElementById("txtIdHora").value;
+
+	if(horaDelDia >= 0 && horaDelDia <=24){
+		switch(horaDelDia){
+			case "7" :
+			case "8" :
+			case "9" :
+			case "10":
+			case "11":
+				alert("Es de mañana");
+			break;
+	
+			case "12" :
+			case "13" :
+			case "14" :
+			case "15":
+			case "16":
+			case "17":
+			case "18":
+			case "19":
+				alert("Es de tarde");
+			break;
+			
+			default:
+				alert("es de noche");
+			break;
+			
+		}}
+	else
+	{alert("La hora no exite");}
+	
 
 }//FIN DE LA FUNCIÓN
