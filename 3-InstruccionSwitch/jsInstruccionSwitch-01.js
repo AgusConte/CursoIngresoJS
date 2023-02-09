c738e5d64a5b92ec9476aad1bcd1e3346e25bd28
agustin conte <agustinconte15@gmail.com>
2023-02-09 10:39:21 -0300

    sw

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,8 +1,29 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño =document.getElementById("txtIdMes").value;
+
+	switch(mesDelAño){
+		case "Enero":
+			alert("Que comienzes bien el año!!!");
+		break;
+
+		case "Marzo":
+			alert("A clases!!!");
+		break;
+
+		case "Julio":
+			alert("Se vienen las vacaciones!!!");
+		break;
+
+		case "Diciembre":
+			alert("Felices fiestas!!!");
+		break;
+
+		default:
+			alert("mes comun");
+		break;
+	}
 
 
 
