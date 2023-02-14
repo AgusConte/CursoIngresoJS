2493234af25831d8a4c60c5e43600acb96311f7a
agustin conte <agustinconte15@gmail.com>
2023-02-14 12:27:17 -0300

    5

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,12 +1,21 @@
 /*
 Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
-function mostrar()
+
+	function mostrar()
 {
-	var sexoIngresado;
-	sexoIngresado = prompt("ingrese f ó m .");
+    let sexoIngresado;
+    sexoIngresado = prompt("ingrese f ó m .");
 
+    while(sexoIngresado != "m" && sexoIngresado != "f"){
+        alert("Ingrese un sexo válido, m o f")
+        sexoIngresado = prompt("ingrese f ó m .")
+    }
+    if(sexoIngresado == "m"){
+        sexoIngresado = "Masculino";
+    }else{
+        sexoIngresado = "Femenino";
+    }
 
-
-	txtIdSexo.value=sexoIngresado;
+    document.getElementById("txtIdSexo").value = sexoIngresado;
 }//FIN DE LA FUNCIÓN
