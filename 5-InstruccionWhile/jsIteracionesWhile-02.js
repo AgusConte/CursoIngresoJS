2e9bf074da003c3ea0bafbceca58f7db212703f3
agustin conte <agustinconte15@gmail.com>
2023-02-16 11:33:07 -0300

    cambio

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,7 +3,7 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	let i
+	/*let i
 
 	i = 10
 
@@ -12,5 +12,39 @@ function mostrar()
 		i = i - 1;
 	}
 
+
+------------------------------------------------------------------------------*/
+
+
+
+	let i;
+	let acumulador;
+	let suma;
+	let promedio;
+	let numeroIngresado;
+
+	i = 0;
+	acumulador=0;
+	numeroIngresado = 0;
+	suma = 0
+	
+	while(i < 5){
+		i = i + 1;
+		numeroIngresado = parseInt(prompt("ingresa un numero"));
+		while(isNaN(numeroIngresado)){
+			numeroIngresado = parseInt(prompt("ERROR : ingresa un numero"));
+		}
+
+		if(numeroIngresado >9 && numeroIngresado <21){
+			acumulador = acumulador + 1;
+			suma = suma + numeroIngresado;
+		}
+		
+	}
+
+	promedio = suma/ acumulador;
+
+	alert(acumulador);
+	alert(promedio);
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,7 +3,7 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	
+	/*
 	let banderaDelPrimero;
 	let numeroIngresado;
 	let numeroMaximo;
@@ -43,4 +43,49 @@ function mostrar()
 	
 	document.getElementById("txtIdMaximo").value=numeroMaximo;
 	document.getElementById("txtIdMinimo").value=numeroMinimo;
+
+
+-----------------------------------------------------------------------------*/
+
+let numeroIngresado;
+let numeroMaximo;
+let numeroMinimo;
+let menorPar;
+let mayorNegativo;
+let respuesta;
+
+maximo = -Infinity;
+minimo = Infinity;
+menorPar = Infinity;
+mayorNegativo = -Infinity;
+respuesta = "si"
+
+
+while(respuesta == "si" || respuesta == "SI" || respuesta == "Si" ||respuesta == "sI"){
+	numeroIngresado = parseInt(prompt("ingrese numero"));
+	while(isNaN(numeroIngresado)){
+		numeroIngresado = parseInt(prompt("ERROR : ingresa un numero"));
+	}
+	respuesta = prompt("¿Desea continuar ingresando números? (coloque si)");
+
+	
+	if( numeroIngresado > numeroMaximo){
+		numeroMaximo = numeroIngresado;
+	}
+
+	else if( numeroIngresado < numeroMinimo ){
+		numeroMinimo = numeroIngresado;
+	}
+
+}
+
+
+document.getElementById("txtIdMaximo").value=numeroMaximo;
+document.getElementById("txtIdMinimo").value=numeroMinimo;
+
+
+alert("El menor de los pares es: " + menorPar);
+alert("El mayor de los negativos es: " + mayorNegativo);
+
+
 }//FIN DE LA FUNCIÓN
