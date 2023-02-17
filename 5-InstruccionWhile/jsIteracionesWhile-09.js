4daf960f2db2403b4e7956fa69c36c26e347210f
agustin conte <agustinconte15@gmail.com>
2023-02-17 19:26:56 -0300

    9 mal

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -46,46 +46,65 @@ function mostrar()
 
 
 -----------------------------------------------------------------------------*/
+let banderaDelPrimero;
+	let numeroIngresado;
+	let numeroMaximo;
+	let numeroMinimo;
+	let menoresPares;
+	let mayoresNegativos;
+	let respuesta;
+	//iniciar variables
+	
+	banderaDelPrimero = true
+	respuesta="si";
+	numeroMaximo = -Infinity;
+	numeroMinimo = Infinity;
+	mayoresNegativos = -Infinity;
+	menoresPares = Infinity;
 
-let numeroIngresado;
-let numeroMaximo;
-let numeroMinimo;
-let menorPar;
-let mayorNegativo;
-let respuesta;
+	while(respuesta == "si" || respuesta == "SI" || respuesta == "Si" ||respuesta == "sI"){
+		numeroIngresado = parseInt(prompt("ingrese numero"));
+		while(isNaN(numeroIngresado)){
+			numeroIngresado = parseInt(prompt("ERROR : ingresa un numero"));
+		}
+	
+		if(banderaDelPrimero == true){
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+			banderaDelPrimero = false;
+		}
 
-maximo = -Infinity;
-minimo = Infinity;
-menorPar = Infinity;
-mayorNegativo = -Infinity;
-respuesta = "si"
+		else if( numeroIngresado > numeroMaximo){
+			numeroMaximo = numeroIngresado;
+		}
+	
+		else if( numeroIngresado < numeroMinimo ){
+			numeroMinimo= numeroIngresado;
+		}
+
+		else if(numeroIngresado %2 == 0){
+			if( numeroIngresado < menoresPares){
+				menoresPares = numeroIngresado
+			}
+			
+		}
+		
+		else if( numeroIngresado < 0){
+			if(numeroIngresado > mayoresNegativos){
+				mayoresNegativos = numeroIngresado
+			}
+		}
 
 
-while(respuesta == "si" || respuesta == "SI" || respuesta == "Si" ||respuesta == "sI"){
-	numeroIngresado = parseInt(prompt("ingrese numero"));
-	while(isNaN(numeroIngresado)){
-		numeroIngresado = parseInt(prompt("ERROR : ingresa un numero"));
+
+		respuesta = prompt("Desea continuar? (coloque Si)");
+	
 	}
-	respuesta = prompt("¿Desea continuar ingresando números? (coloque si)");
 
 	
-	if( numeroIngresado > numeroMaximo){
-		numeroMaximo = numeroIngresado;
-	}
-
-	else if( numeroIngresado < numeroMinimo ){
-		numeroMinimo = numeroIngresado;
-	}
-
-}
-
-
-document.getElementById("txtIdMaximo").value=numeroMaximo;
-document.getElementById("txtIdMinimo").value=numeroMinimo;
-
-
-alert("El menor de los pares es: " + menorPar);
-alert("El mayor de los negativos es: " + mayorNegativo);
-
-
+	document.getElementById("txtIdMaximo").value=numeroMaximo;
+	document.getElementById("txtIdMinimo").value=numeroMinimo;
+	alert("m"+menoresPares);
+	alert("x"+mayoresNegativos);
+	
 }//FIN DE LA FUNCIÓN
